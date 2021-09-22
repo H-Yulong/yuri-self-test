17261fa9e7dde3c59b9cea1c4161bd687bcfa3e5
Yulong Huang <jasonjostar@gmail.com>
2021-09-22 22:16:40 +0800

    Initial commit

---
+++ questions.js
@@ -0,0 +1,497 @@
+questions = [
+    {
+        "question": "百合就是女同性恋的同义词。",
+        "effect": {
+            "econ": -2,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "现在大部分的百合作品，都在曲解百合的原来含义。",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -2,
+            "scty": 0
+        }
+    },
+    {
+        "question": "只有女女之间的感情才算百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合应该是纯精神的，和肉欲无关。",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合是迎合死宅喜好的商品。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "动画中的百合情侣，和现实里的蕾丝情侣差不多。",
+        "effect": {
+            "econ": -2,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "百合应该留在小圈子内，不要广泛传播。",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合作品应该致力展现女性的美。",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "对于动画作品，我可以接受故意加入百合元素的“百合营业”行为。",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "对于现实/虚拟主播的直播、视频，我可以接受故意加入百合元素的“百合营业”行为。",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我反对一般向作品的百合同人创作。",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "只要角色有百合苗头，我就可以在同人作品里凑她们的cp。",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "（某些情况下）就算没有百合苗头，我也可以在同人作品里凑cp。",
+        "effect": {
+            "econ": 0,
+            "dipl": -20,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "能凑百合cp的话，稍微OOC也无所谓。",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "亲密的肢体接触就是百合的苗头。",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "普通的日常动画里，可以加入百合元素，另有风味还能吸引观众。",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "轻百合日常番是好文明，它帮助我放松。",
+        "effect": {
+            "econ": 0,
+            "dipl": -2,
+            "govt": 10,
+            "scty": 0 
+        }
+    },
+    {
+        "question": "真正的女同性恋根本不看百合番。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "喜欢过男人的女角色不配搞百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "“因为是你才喜欢，不论你是什么性别都喜欢”，这么想的女女cp不算百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0 
+        }
+    },
+    {
+        "question": "与其他男性发生过性关系的女角色不配搞百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合更多是一种细腻、温柔的友情。",
+        "effect": {
+            "econ": 10,
+            "dipl": 2,
+            "govt": -2,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合cp间的爱是要有来头的，不能凭空捏造cp感情。",
+        "effect": {
+            "econ": -2,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我不介意看黛玉和宝钗的全年龄百合同人。（注：林黛玉、薛宝钗出自清代小说《红楼梦》，原著中她们是亲密的交心好友，但两人显然都是异性恋。原作中，她们的感情是惺惺相惜的姐妹情、友情。）",
+        "effect": {
+            "econ": 2,
+            "dipl": -5,
+            "govt": 2,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我不介意看小樱和知世的百合同人。（注：木之本樱、大道寺知世出自CLAMP所作的漫画《百变小樱》。原作中她们是好朋友，但知世对小樱的感情是比友情要高的、“希望对方幸福”的“爱”。）",
+        "effect": {
+            "econ": -2,
+            "dipl": -5,
+            "govt": 2,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我不介意看小圆和小焰的百合同人。（注：鹿目圆、晓美焰出自新房昭之监督的动画《魔法少女小圆》。原作中，小焰对小圆有着强烈的爱慕心、占有欲。）",
+        "effect": {
+            "econ": -2,
+            "dipl": -5,
+            "govt": 2,
+            "scty": 0
+        }
+    },
+    {
+        "question": "四、五十岁的阿姨cp显然不算百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我喜欢《圣母在上》那样的“清水百合”作品（即着重描述少女精神依存的作品）。",
+        "effect": {
+            "econ": 2,
+            "dipl": 5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "我喜欢《少女革命》那样晦涩、难懂，但反应了社会现象的作品。",
+        "effect": {
+            "econ": 2,
+            "dipl": -2,
+            "govt": -10,
+            "scty": -2
+        }
+    },
+    {
+        "question": "描写细腻的百合感情，不比制作宏大的动作场面、机战、爆炸之类的简单。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "与LGBT运动一样，百合作品可以帮助性少数群体夺回公众话语权。",
+        "effect": {
+            "econ": -2,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "一部作品要么专注讲百合，要么不讲。",
+        "effect": {
+            "econ": 0,
+            "dipl": 2,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合作品里不应该有性的描写。",
+        "effect": {
+            "econ": 2,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "百合之间的感情也要专一，百合cp之间不允许有第三者出现。",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 2,
+            "scty": -2
+        }
+    },
+    {
+        "question": "百合的主题永远是：妙龄少女以细腻的心思、感情和人文关怀，对抗她们生活在的灰暗、压抑的环境。",
+        "effect": {
+            "econ": 2,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合番的流行，可以让大众对女同性恋群体更包容。",
+        "effect": {
+            "econ": -2,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "百合cp中，一方可以是伪娘（带把）。",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合cp中，双方都可以是伪娘（带把）。",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合cp中，可以有大大咧咧的女汉子角色。",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": -2
+        }
+    },
+    {
+        "question": "百合cp中，一方/双方可以是扶她。",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合作品只能由女性来做。",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "百合是可以存在于多人之间的，三、四个女生之间非常要好也算百合。",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+    {
+        "question": "在一般向的作品里加入百合元素是不好的。",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "二次元的“百合”和三次元的女同性恋完全不是一码事。",
+        "effect": {
+            "econ": 2,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "处于SM关系中的女同性恋cp算是百合。",
+        "effect": {
+            "econ": 0,
+            "dipl": -2,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "姐妹之间的亲情也是百合。",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": -2,
+            "scty": 2
+        }
+    },
+    {
+        "question": "师徒之间的情谊也可以是百合。",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": -2,
+            "scty": 2
+        }
+    },
+    {
+        "question": "前辈对后辈的关怀、爱护也是百合。",
+        "effect": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": -2,
+            "scty": 2
+        }
+    },
+    {
+        "question": "百合作品里不应该有太多性的暗示",
+        "effect": {
+            "econ": 2,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 2
+        }
+    },
+    {
+        "question": "“百合”观念本质上还是取悦男性，是媚宅行为。",
+        "effect": {
+            "econ": 0,
+            "dipl": 2,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "铁t女同也是百合。",
+        "effect": {
+            "econ": -2,
+            "dipl": -2,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "百合旨在颠覆男性主导的恋爱观。",
+        "effect": {
+            "econ": -2,
+            "dipl": 2,
+            "govt": -2,
+            "scty": -10
+        }
+    },
+    {
+        "question": "百合作品里不能有男性角色出现。",
+        "effect": {
+            "econ": 0,
+            "dipl": 2,
+            "govt": 2,
+            "scty": -5
+        }
+    },
+    {
+        "question": "百合是一种复杂的文化现象,不能和LGBT混为一谈。",
+        "effect": {
+            "econ": 0,
+            "dipl": 2,
+            "govt": -2,
+            "scty": 10
+        }
+    },
+    {
+        "question": "“百合”文化是一种短暂的流行趋势，和它相关的作品没有什么艺术深度。",
+        "effect": {
+            "econ": 0,
+            "dipl": -2,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+];
